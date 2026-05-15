cab35aa244fcb5ef050c0d9e0a5730223c8faf50
Manasa <manasapatil2432005@gmail.com>
2026-05-15 20:51:03 +0530

    suggestion feature added

--- src/app.py
+++ src/app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import re
+from difflib import get_close_matches
 
 # Synonym dictionary
 synonyms = {
@@ -16,16 +18,26 @@ synonyms = {
 # Load dataset
 df = pd.read_csv("dataset/agro_terms.csv")
 
+keywords = df["keyword"].tolist()
+
 st.title("🌾 AgroSign AI")
 
 text = st.text_input("Enter agriculture text:")
 
 if text:
-    words = text.lower().split()
+   
+
+    # Remove punctuation and split properly
+    clean_text = re.sub(r'[^\w\s]', '', text.lower())
+    words = clean_text.split()
+
+    stopwords = ["is", "the", "in", "are", "and", "of", "to"]
+
+    words = [word for word in words if word not in stopwords]
 
     st.subheader("Detected Signs:")
 
-    for word in words:   # ✅ NOW INSIDE
+    for word in words:
 
         # Basic normalization
         if word.endswith("ing"):
@@ -34,7 +46,7 @@ if text:
         if word.endswith("s"):
             word = word[:-1]
 
-        # Apply synonym mapping
+        # Synonym mapping
         if word in synonyms:
             word = synonyms[word]
 
@@ -44,3 +56,10 @@ if text:
             video_path = match.iloc[0]["video_path"]
             st.write(f"👉 {word}")
             st.video(video_path)
+
+        else:
+            # 🔥 Smart suggestion part
+            suggestion = get_close_matches(word, keywords, n=1, cutoff=0.6)
+
+            if suggestion:
+                st.write(f"❓ Did you mean: **{suggestion[0]}**?")

4260d1c502bca3f01eb615cda69ea749a8cd934c
Lakshita <lakshitabinyala@gmail.com>
2026-05-15 20:26:26 +0530

    changed file path in app and add more keywords in synonyms

--- src/app.py
+++ src/app.py
@@ -1,20 +1,59 @@
 import streamlit as st
 import pandas as pd
+import os
 
 # Synonym dictionary
 synonyms = {
-    "plants": "crop",
-    "plant": "crop",
-    "harvest": "crop",
-    "harvesting": "crop",
+
+    # Crops / Plants
+    "plant": "crops",
+    "plants": "crops",
+    "crop": "crops",
+    "harvest": "harvesting",
+    "harvesting": "harvesting",
+    "seedling": "seed",
+    "seeds": "seed",
+    "farming": "farm",
+    "agriculture": "farm",
+
+    # Water / Irrigation
     "irrigation": "water",
     "watering": "water",
+    "pump": "machine_water_pump",
+    "handpump": "handpump_water",
+
+    # Farmers / Farm
     "farms": "farm",
-    "farmers": "farmer"
+    "farmers": "farmer",
+    "cultivation": "farm",
+
+    # Machines
+    "tractoring": "tractor",
+    "plowing": "plough",
+    "ploughing": "plough",
+    "machine": "machine_water_pump",
+    "harvester": "harvester",
+
+    # Animals
+    "buffaloes": "buffalo",
+    "cows": "cow",
+    "goats": "goat",
+    "hens": "chicken",
+
+    # Agriculture Materials
+    "fertilizers": "fertilizer",
+    "pesticides": "pesticide",
+    "organic": "organic_food",
+
+    # Weather / Soil
+    "climate": "weather",
+    "rain": "weather",
+    "mud": "soil",
+    "land": "soil"
 }
 
 # Load dataset
-df = pd.read_csv("dataset/agro_terms.csv")
+df = pd.read_csv("../dataset/agro_terms.csv")
 
 st.title("🌾 AgroSign AI")
 
@@ -41,6 +80,7 @@ if text:
         match = df[df["keyword"] == word]
 
         if not match.empty:
-            video_path = match.iloc[0]["video_path"]
-            st.write(f"👉 {word}")
-            st.video(video_path)
+           video_path = match.iloc[0]["video_path"]
+           video_path = os.path.join("..", video_path)
+           st.write(f"👉 {word}")
+           st.video(video_path)
